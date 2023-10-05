89eecd84860287b0925db26b3bdfa72c87fdb1d1
kyungkwon01 <kyungkwon01@gmail.com>
2023-10-04 18:11:05 -0600

    add project

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,47 +1,48 @@
 import ProductivityNinja from '../assets/Productivity-Ninja.png';
 import FiveDayWeatherForecast from '../assets/5-Day-Weather-Forecast.png';
+import EmployMeNow from '../assets/employmenow.png';
 
 export const data = [
-  {
-    id: 1,
-    name: "Productivity Ninja",
-    image: ProductivityNinja,
-    github: "https://github.com/ChadBowler/Productivity-Ninja",
-    live: "https://productivity-ninja-d6cc6908ff4d.herokuapp.com/",
-  },
-  {
-    id: 2,
-    name: "5 Day Weather Forecast",
-    image: FiveDayWeatherForecast,
-    github: "https://github.com/kyungkwon01/5-day-weather-forecast",
-    live: "https://kyungkwon01.github.io/5-day-weather-forecast/",
-  },
-  {
-    id: 3,
-    name: "Productivity Ninja",
-    image: ProductivityNinja,
-    github: "https://github.com/ChadBowler/Productivity-Ninja",
-    live: "https://productivity-ninja-d6cc6908ff4d.herokuapp.com/",
-  },
-  {
-    id: 4,
-    name: "5 Day Weather Forecast",
-    image: FiveDayWeatherForecast,
-    github: "https://github.com/kyungkwon01/5-day-weather-forecast",
-    live: "https://kyungkwon01.github.io/5-day-weather-forecast/",
-  },
-  {
-    id: 5,
-    name: "Productivity Ninja",
-    image: ProductivityNinja,
-    github: "https://github.com/ChadBowler/Productivity-Ninja",
-    live: "https://productivity-ninja-d6cc6908ff4d.herokuapp.com/",
-  },
-  {
-    id: 6,
-    name: "5 Day Weather Forecast",
-    image: FiveDayWeatherForecast,
-    github: "https://github.com/kyungkwon01/5-day-weather-forecast",
-    live: "https://kyungkwon01.github.io/5-day-weather-forecast/",
-  }
+	{
+		id: 1,
+		name: 'Productivity Ninja',
+		image: ProductivityNinja,
+		github: 'https://github.com/ChadBowler/Productivity-Ninja',
+		live: 'https://productivity-ninja-d6cc6908ff4d.herokuapp.com/',
+	},
+	{
+		id: 2,
+		name: '5 Day Weather Forecast',
+		image: FiveDayWeatherForecast,
+		github: 'https://github.com/kyungkwon01/5-day-weather-forecast',
+		live: 'https://kyungkwon01.github.io/5-day-weather-forecast/',
+	},
+	{
+		id: 3,
+		name: 'Employ Me Now',
+		image: EmployMeNow,
+		github: 'https://github.com/ChadBowler/employ-me-now',
+		live: 'https://employ-me-now-d264f478796e.herokuapp.com/',
+	},
+	{
+		id: 4,
+		name: '5 Day Weather Forecast',
+		image: FiveDayWeatherForecast,
+		github: 'https://github.com/kyungkwon01/5-day-weather-forecast',
+		live: 'https://kyungkwon01.github.io/5-day-weather-forecast/',
+	},
+	{
+		id: 5,
+		name: 'Productivity Ninja',
+		image: ProductivityNinja,
+		github: 'https://github.com/ChadBowler/Productivity-Ninja',
+		live: 'https://productivity-ninja-d6cc6908ff4d.herokuapp.com/',
+	},
+	{
+		id: 6,
+		name: '5 Day Weather Forecast',
+		image: FiveDayWeatherForecast,
+		github: 'https://github.com/kyungkwon01/5-day-weather-forecast',
+		live: 'https://kyungkwon01.github.io/5-day-weather-forecast/',
+	},
 ];
